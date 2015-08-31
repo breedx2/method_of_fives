86bc79b98f446545f647a8704848d0d98e36ccc1
Jason Plumb <jason@noisybox.net>
2015-08-31 00:02:05 -0700

    Rough in frame generator based on pats from framer

---
+++ frame_generator.py
@@ -0,0 +1,53 @@
+import subprocess as sp
+import numpy
+from metadata import VideoMeta
+
+class FrameGenerator(object):
+
+	@staticmethod
+	def build(ffprobe, ffmpeg, filename, bpp):
+		meta = VideoMeta(ffprobe).read(filename)
+		return FrameGenerator(ffmpeg, bpp, filename, meta)
+
+	def __init__(self, ffmpeg, bpp, filename, meta):
+		self.ffmpeg = ffmpeg
+		self.bpp = bpp # bytes per pixel
+		self.filename = filename
+		self.meta = meta
+		self.pipe = None
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		return self.next()
+
+	def next(self):
+		self._lazy_init()
+		raw_image = self.pipe.stdout.read(self.meta['width'] * self.meta['height'] * self.bpp)
+		image =  numpy.fromstring(raw_image, dtype='uint8')
+		image = image.reshape(self.meta['height'], self.meta['width'])
+		self.pipe.stdout.flush()
+		# TODO: Determine safe finishing/done state
+		if(False):
+			raise StopIteration()
+		return image
+
+	def _lazy_init(self):
+		if not self.pipe:
+			command = [ self.ffmpeg,
+	            '-i', self.filename,
+	            '-f', 'image2pipe',
+	            '-pix_fmt', self._ffmpeg_format(),
+	            '-vcodec', 'rawvideo', '-']
+			self.pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+
+	def _ffmpeg_format(self):
+		if(self.bpp == 1):
+			return 'gray'
+		if(eslf.bpp == 3):
+			return 'rgb24'
+		raise Exception('Unhandled byte depth: %d' %(self.bpp))
+
+	def _framebuf_size(self):
+		return self.meta['width'] * self.meta['width'] * self.bpp
--- framer.py
+++ framer.py
@@ -2,12 +2,13 @@ import sys
 import subprocess as sp
 import numpy
 from metadata import VideoMeta
+from frame_generator import FrameGenerator
 
 FFMPEG_BIN = 'ffmpeg'
 FFPROBE_BIN = 'ffprobe'
 
 def usage():
-	print("Usage: %s <filename> <width> <height>" % (sys.argv[0]))
+	print("Usage: %s <filename>" % (sys.argv[0]))
 	sys.exit(1)
 
 if len(sys.argv) < 2:
@@ -19,43 +20,47 @@ for k,v in meta.items():
 	print("%s: %s" % (k,v))
 print("----------------------------")
 
-#sys.exit(0)	# debuggery
-
 filename = sys.argv[1]
-# width = sys.argv[2]
-# height = sys.argv[3]
 
-command = [ FFMPEG_BIN,
-            '-i', filename,
-            '-f', 'image2pipe',
-            '-pix_fmt', 'gray',
-            '-vcodec', 'rawvideo', '-']
-pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
+framegen = FrameGenerator.build(FFPROBE_BIN, FFMPEG_BIN, filename, 1)
 
-# read 1 frame (assumes 3 bytes per pixel)
-# raw_image = pipe.stdout.read(meta['width'] * meta['height'] * 3)
-raw_image = pipe.stdout.read(meta['width'] * meta['height'] * 1) # 1 byte per pixel with pix_fmt gray
+# command = [ FFMPEG_BIN,
+#             '-i', filename,
+#             '-f', 'image2pipe',
+#             '-pix_fmt', 'gray',
+#             '-vcodec', 'rawvideo', '-']
+# pipe = sp.Popen(command, stdout = sp.PIPE, bufsize=10**8)
 
-# Test that the frame looks right by saving it to a raw .data file
-# f = open('/tmp/outfile.data', 'w')
-# for i in range(0, meta['width'] * meta['height']):
-# 	f.write(raw_image[i])
-# 	f.write(raw_image[i])
-# 	f.write(raw_image[i])
-# f.close()
+for i in range(0,10):
 
-image =  numpy.fromstring(raw_image, dtype='uint8')
-image = image.reshape((meta['height'], meta['width']))
-pipe.stdout.flush()
+	# read 1 frame (assumes 3 bytes per pixel)
+	# raw_image = pipe.stdout.read(meta['width'] * meta['height'] * 3)
+	# raw_image = pipe.stdout.read(meta['width'] * meta['height'] * 1) # 1 byte per pixel with pix_fmt gray
 
-f = open('/tmp/outfile.data', 'w')
-for y in range(0, meta['height']):
-	for x in range(0, meta['width']):
-		f.write(chr(image[y, x]))
-		f.write(chr(image[y, x]))
-		f.write(chr(image[y, x]))
-f.close()
+	# Test that the frame looks right by saving it to a raw .data file
+	# f = open('/tmp/outfile.data', 'w')
+	# for i in range(0, meta['width'] * meta['height']):
+	# 	f.write(raw_image[i])
+	# 	f.write(raw_image[i])
+	# 	f.write(raw_image[i])
+	# f.close()
 
 
-# print(">>>>>>>>>>>>>>>>>>> %d %d %d" % (image[212,210,0], image[212,210,1], image[210,212,2]))
+
+	# image =  numpy.fromstring(raw_image, dtype='uint8')
+	# image = image.reshape((meta['height'], meta['width']))
+	# pipe.stdout.flush()
+
+	image = framegen.next()
+
+	f = open('/tmp/outfile%d.data' % (i), 'w')
+	for y in range(0, image.shape[0]):	# height
+		for x in range(0, image.shape[1]):
+			f.write(chr(image[y, x]))
+			f.write(chr(image[y, x]))
+			f.write(chr(image[y, x]))
+	f.close()
+
+
+	# print(">>>>>>>>>>>>>>>>>>> %d %d %d" % (image[212,210,0], image[212,210,1], image[210,212,2]))
 
