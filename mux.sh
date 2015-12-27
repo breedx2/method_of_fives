0ec1b99864feb98221daad1cf0c3ec295d44fed7
Jason Plumb <jason@noisybox.net>
2015-12-27 00:02:37 -0800

    Mux to exactly 5 minutes

--- mux.sh
+++ mux.sh
@@ -5,4 +5,4 @@
 AUDIO=$1
 VIDEO=/home/jason/media/VID_20150626_191903.mp4
 
-ffmpeg -i ${VIDEO} -i ${AUDIO} -strict -2 -c copy -acodec mp3 -q:a 0 out.mp4
+ffmpeg -i ${VIDEO} -i ${AUDIO} -strict -2 -c copy -acodec mp3 -q:a 0 -t 00:05:00.0 out.mp4
